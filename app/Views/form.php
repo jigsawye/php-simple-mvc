10df81835f99ef328cec1302e0ff270267fb0be8
jigsawye <jigsaw.ye@gmail.com>
2015-06-03 18:23:18 +0800

    將表單Html移至View資料夾，並透過require在route載入

---
+++ app/Views/form.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home Form</title>
+</head>
+<body>
+    <form action="./index.php/test" method="post">
+        <input type="text" name="test_value">
+        <input type="submit">
+    </form>
+</body>
+</html>
--- app/route.php
+++ app/route.php
@@ -2,11 +2,7 @@
     $router = new Router;
 
     $router->get('/home', function () {
-        // 以下為不良示範，不要亂學
-        echo '<form action="./index.php/test" method="post">';
-        echo '<input type="text" name="test_value">';
-        echo '<input type="submit">';
-        echo '</form>';
+        require_once __DIR__ . '/../app/Views/form.php';
     });
 
     $router->post('/test', function () {
