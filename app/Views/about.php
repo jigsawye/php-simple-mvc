9fd3cd20f359ffecfc53ec8fd9978b8a2d29eebc
jigsawye <jigsaw.ye@gmail.com>
2015-06-04 02:05:12 +0800

    修改三個View的樣式及版型

--- app/Views/about.php
+++ app/Views/about.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>About</title>
+    <title>About@Fake-Blog</title>
     <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+
     <nav class="navbar navbar-default navbar-static-top">
         <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="/">Fake-Blog</a>
+            </div>
             <div class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
+                <ul class="nav navbar-nav navbar-right">
                     <li><a href="/">Home</a></li>
                     <li class="active"><a href="/about">About</a></li>
                     <li><a href="/contact">Contact</a></li>
@@ -16,8 +20,12 @@
             </div>
         </div>
     </nav>
-    <div class="container">
-        <h1>About</h1>
+
+    <div class="jumbotron">
+        <div class="container">
+            <h1 class="text-center">About Page</h1>
+        </div>
     </div>
+
 </body>
 </html>
--- app/Views/index.php
+++ app/Views/index.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Home</title>
+    <title>Home@Fake-Blog</title>
     <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+
     <nav class="navbar navbar-default navbar-static-top">
         <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="/">Fake-Blog</a>
+            </div>
             <div class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
+                <ul class="nav navbar-nav navbar-right">
                     <li class="active"><a href="/">Home</a></li>
                     <li><a href="/about">About</a></li>
                     <li><a href="/contact">Contact</a></li>
@@ -16,8 +20,12 @@
             </div>
         </div>
     </nav>
-    <div class="container">
-        <h1>Home</h1>
+
+    <div class="jumbotron">
+        <div class="container">
+            <h1 class="text-center">Home Page</h1>
+        </div>
     </div>
+
 </body>
 </html>
