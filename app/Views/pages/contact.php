244202fc4df4f4d5bae137ba7e11eee900afeb25
jigsawye <jigsaw.ye@gmail.com>
2015-06-08 00:44:26 +0800

    更新Contact的View

--- app/Views/pages/contact.php
+++ app/Views/pages/contact.php
@@ -23,9 +23,42 @@
         </div>
     </nav>
 
-    <div class="jumbotron">
-        <div class="container">
-            <h1 class="text-center">Contact Page</h1>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="page-header text-center">
+                <h3>Contact Us</h3>
+            </div>
+            <form class="form-horizontal" role="form" method="POST" action="">
+
+                <div class="form-group">
+                    <label class="col-md-4 control-label">Your Name</label>
+                    <div class="col-md-6">
+                        <input type="email" class="form-control" name="name" value="">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-md-4 control-label">E-Mail Address</label>
+                    <div class="col-md-6">
+                        <input type="email" class="form-control" name="email" value="">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-md-4 control-label">Password</label>
+                    <div class="col-md-6">
+                        <textarea class="form-control" name="message" rows="4"></textarea>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-md-6 col-md-offset-4">
+                        <button type="submit" class="btn btn-primary">
+                            Send
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 
