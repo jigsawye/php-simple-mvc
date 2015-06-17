50b62539cdd6bd39f2213c9936d13b1856abe5bf
jigsawye <jigsaw.ye@gmail.com>
2015-06-17 14:25:13 +0800

    加入Posts的routes及PostsCntroller

---
+++ app/Controllers/PostsController.php
@@ -0,0 +1,74 @@
+<?php
+
+class PostsController
+{
+    /**
+     * 顯示所有文章
+     *
+     * @return View [description]
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * 顯示單一文章
+     *
+     * @return View [description]
+     */
+    public function show()
+    {
+
+    }
+
+    /**
+     * 顯示建立文章的表單
+     *
+     * @return View [description]
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * 接收建立文章表單的資料並寫入資料庫
+     *
+     * @return Redirect [description]
+     */
+    public function store()
+    {
+
+    }
+
+    /**
+     * 顯示編輯文章的表單
+     *
+     * @return View [description]
+     */
+    public function edit()
+    {
+
+    }
+
+    /**
+     * 接收更新文章表單的資料並更新資料庫
+     *
+     * @return Redirect [description]
+     */
+    public function update()
+    {
+
+    }
+
+    /**
+     * 刪除單一文章
+     *
+     * @return Redirect [description]
+     */
+    public function delete()
+    {
+
+    }
+}
